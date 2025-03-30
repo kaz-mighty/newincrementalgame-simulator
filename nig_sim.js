dc75bc75499951a2abdc8893ea20ae94ec8e167b
kaz-mighty <kaz-mighty@users.noreply.github.com>
2025-03-30 13:59:11 +0000

    deploy: 77d60d81a11e32b89238195452e318f34810b8d1

--- nig_sim.js
+++ nig_sim.js
@@ -2328,6 +2328,7 @@ const initialConfig = () => {
             doubleUp: false,
             minPoint: '0',
             maxPoint: '1e100',
+            showGainLevel: false,
         },
         procMsPerTick: 0,
         verbose: false,
@@ -2490,6 +2491,9 @@ const app = Vue.createApp({
         },
         chipCheckpointMoney() {
             return new Array(itemData.chipTable.length).fill(null).map((_, i) => this.nig.getGainChipMoney(i));
+        },
+        chipCheckpointLevel() {
+            return this.chipCheckpointMoney.map(x => this.nig.calcGainLevel(x).toExponential(3));
         },
         chipCheckpointRange() {
             let min = D(0);
@@ -2897,26 +2901,24 @@ const app = Vue.createApp({
                 'btn-outline-dark': !cond,
             };
         },
-        chipColoredButtonClass(j) {
+        chipColoredButtonStyle(j) {
             if (j === 0) {
                 return {};
-            } else if (j >= 9) {
-                let color = ['silver', 'gold'][j - 9];
-                return {
-                    'background-color': color,
-                    'background-image': 'linear-gradient(135deg,transparent 20%,40%,rgba(255,255,255,1) 50%,60%,transparent 80%)'
-                }
-
-            } else {
-                let color = [
-                    '#cd7f32', 'silver', 'gold',
-                    '#E5E4E2', '#EE82EE', '#FF3333',
-                    '#42FFDD', '#38B48B'
-                ][j - 1];
-                return {
-                    'background-color': color,
-                };
             }
+            let color = [
+                '#cd7f32', 'silver', 'gold',
+                '#E5E4E2', '#EE82EE', '#FF3333',
+                '#42FFDD', '#38B48B',
+                'silver', 'gold',
+            ][j - 1];
+            let image = 'none';
+            if (j >= 9) {
+                image = 'linear-gradient(135deg,transparent 20%,40%,rgba(255,255,255,1) 50%,60%,transparent 80%)';
+            }
+            return {
+                'background-color': color,
+                'background-image': image,
+            };
         },
         scaleChallengeTable(c) {
             this.config.simulateTableWidth = Math.max(20, Math.min(100, this.config.simulateTableWidth * c));
