8d41ba0bca42f3b8acc4f1460587fa425e7f0d5f
kaz_mighty <kaz8032@gmail.com>
2025-03-31 21:19:03 +0900

    add 世界管効果ツールチップ

--- nig_sim.js
+++ nig_sim.js
@@ -102,7 +102,16 @@ class ItemData {
             '階位挑戦を200種類以上達成する',
         ];
         this.worldPipeText = [
-
+            {effect: "発生器コスト", value: (x) => D(-(x * 0.2)).pow_base(10).toExponential(3) + " 倍"},
+            {effect: "時間加速器生産量", value: (x) => (1 + x * 0.2).toFixed(1) + " 倍"},
+            {effect: "段位取得量", value: (x) => D(x / 5.0).pow_base(2).toFixed(1) + " 倍"},
+            {effect: "時間加速器iコスト", value: (x) => "10 ^ (" + (-x * 0.2).toFixed(1) + " * i) 倍"},
+            {effect: "階位取得量", value: (x) => (1 + x * 0.2).toFixed(1) + " 倍"},
+            {effect: "裏発生器生産量", value: (x) => (1 + x * 0.2).toFixed(1) + " 倍"},
+            {effect: "輝き入手確率", value: (x) => "+ " + x + " %"},
+            {effect: "鋳片判定ライン", value: (x) => D(10).pow(-(x * 0.4)).toExponential(3) + " 倍"},
+            {effect: "裏発生器iコスト", value: (x) => "10 ^ (" + (-x * 0.02).toFixed(2) + " * i * i) 倍"},
+            {effect: "煌き入手確率", value: (x) => "+ " + (0.1 * x * 0.5).toFixed(2) + " %"},
         ];
         this.trophyText = [
             '有段者',
@@ -1773,18 +1782,21 @@ class Nig {
             this.memory += this.players[i].trophies.reduce((x, y) => x + (y ? 1 : 0), 0);
         }
     };
+    getPipedSmallMemory(fromWorld, pipeNum) {
+        let count = (this.smallMemories[fromWorld] - 75) * pipeNum;
+        const remember = this.players[fromWorld].remember;
+        if (remember >= 10) {
+            count = Math.floor(count * (0.1 + remember / 10));
+        }
+        return count;
+    };
     checkPipedSmallMemories() {
         let sum = 0;
         for (let i = 0; i < 10; i++) {
             if (this.players[i].worldPipe[this.world] >= 1) {
-                let cnt = this.smallMemories[i];
-                cnt -= 75;
-                cnt *= this.players[i].worldPipe[this.world];
-                if (this.players[i].remember >= 10) {
-                    cnt = Math.floor(cnt * (0.1 + this.players[i].remember / 10));
-                }
-                this.eachPipedSmallMemory[i] = cnt;
-                sum += cnt;
+                const count = this.getPipedSmallMemory(i, this.players[i].worldPipe[this.world]);
+                this.eachPipedSmallMemory[i] = count;
+                sum += count;
             } else {
                 this.eachPipedSmallMemory[i] = 0;
             }
